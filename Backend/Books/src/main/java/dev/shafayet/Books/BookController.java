dadc98ba0c555f27dc34d4ab8f2ddfa56a6af7bc
kanon <kanon8308@gmail.com>
2023-04-10 23:48:00 +0530

    revision

--- Backend/Books/src/main/java/dev/shafayet/Books/BookController.java
+++ Backend/Books/src/main/java/dev/shafayet/Books/BookController.java
@@ -1,16 +1,24 @@
-package dev.shafayet.Books;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1/books")
-public class BookController {
-    @GetMapping
-    public ResponseEntity<String> getAllBooks(){
-        return new ResponseEntity<String>("All Books", HttpStatus.OK);
-    }
-}
+//package dev.shafayet.Books;
+//
+//import org.bson.types.ObjectId;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Optional;
+//
+//@RestController
+//@RequestMapping("/api/v1/books")
+//public class BookController {
+//    @GetMapping
+//    public ResponseEntity<String> getAllBooks(){
+//        return new ResponseEntity<List<Book>>(., HttpStatus.OK);
+//    }
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Optional<Book>> getSingleBook(@PathVariable ObjectId Id){
+//        return new ResponseEntity<Optional<Books>>()
+//    }
+//}
--- Backend/Books/src/main/java/dev/shafayet/Books/BookService.java
+++ Backend/Books/src/main/java/dev/shafayet/Books/BookService.java
@@ -1,9 +1,11 @@
 package dev.shafayet.Books;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -11,5 +13,8 @@ public class BookService {
     private BookRepository bookRepository;
     public List<Book> allBooks(){
         return bookRepository.findAll();
+    }
+    public Optional<Book> singleBooks(ObjectId Id){
+        return bookRepository.findById(Id);
     }
 }
